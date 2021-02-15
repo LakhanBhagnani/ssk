43a5c8f3361536b0b5bf568f3ea7954e871f3c76
Sukhbirsingh Khalsa <sukhbirkhalsait@gmail.com>
2021-02-16 01:32:08 +0530

    added new Basics.java file check.

---
+++ Basics.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Basics{
+    static Scanner input = new Scanner(System.in);
+    static void evenOdd(){
+        System.out.println("Enter number to be checked:");
+        int number = input.nextInt();
+        //With if else 
+        if(number %2 == 0){
+            System.out.print("even");
+        }else{
+            System.out.print("odd");
+        }
+        //with conditional operator
+        String result = (number %2 ==0) ? "even" : "odd" ;
+        System.out.print(result);
+
+    }
+    public static void main(String[] args) {
+       evenOdd();
+
+    }
+}
